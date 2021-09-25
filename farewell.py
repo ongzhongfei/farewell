04978723dc84692af54f98a750d0891aad1c6bb6
Ong Zhong Fei <zhongfei@bnm.gov.my>
2021-09-25 23:25:11 +0800

    app update

--- farewell.py
+++ farewell.py
@@ -72,7 +72,7 @@ DSA_member_messages = {
     },
 }
 
-@st.cache()
+@st.experimental_memo#@st.cache()
 def get_jps_messages():
     jps_survey = pd.read_excel("Farewell message to Khairul Nazran.xlsx")
 
@@ -85,7 +85,7 @@ def get_jps_messages():
     #### One word adjectives
     jps_survey[survey_cols[5]] = jps_survey[survey_cols[5]].str.replace("Serious but Not Serious","Serious_but_not_serious")
     jps_survey[survey_cols[5]] = jps_survey[survey_cols[5]].str.replace("(one word not enough ;p)",'')
-    jps_survey[survey_cols[5]] = jps_survey[survey_cols[5]].str.replace("(Is there no problem he can’t fix)",'')
+    jps_survey[survey_cols[5]] = jps_survey[survey_cols[5]].str.replace("(Is there no problem he can’t fix)",'No_problem_he_cant_fix')
     jps_survey[survey_cols[5]] = jps_survey[survey_cols[5]].str.replace("Nice person",'Nice')
 
     jps_survey[survey_cols[5]] = jps_survey[survey_cols[5]].str.replace("?",'',regex=False)
@@ -124,7 +124,7 @@ def grey_color_func(word, font_size, position, orientation, random_state=None,
                     **kwargs):
     return "hsl(0, 0%%, %d%%)" % random.randint(60, 100)
 
-@st.cache(allow_output_mutation=True)
+@st.experimental_memo#@st.cache()
 def get_wordcloud(text):
     naz_mask = np.array(Image.open("pic/naz-removebg-preview.png"))
 
@@ -144,10 +144,26 @@ def page_content(page_number):
         cols = st.columns([1,1,1])
 
         with cols[1]:
-            st.header("Congratulations! and goodbye...")
-            st.write("\n")
+            # st.header("Congratulations! and goodbye...")
             st.write(
-                f"""<p style="text-align:center;"><img src="https://media3.giphy.com/media/6nuiJjOOQBBn2/200w.webp?cid=ecf05e47xcmnv9n8vasc1d01665fpyw6u8qee6tg305wyews&rid=200w.webp&ct=g" style="width:500px;height:450px;"></p>""",
+                f"""<span style="color:#00FFFF;font-size:40px"> <b> Congratulations! and goodbye... </b></span>""",
+                unsafe_allow_html=True,
+            )
+            st.write("\n")
+
+            st.subheader("The Usual Naz... :male-office-worker:")
+            naz_office = Image.open('naz_photos/naz_office.jpg')
+            st.image(naz_office, width=455)
+            naz_pp = Image.open('naz_photos/naz.png')
+            st.image(naz_pp, width=380)
+
+            st.write("<br>",unsafe_allow_html=True)
+            st.write("<br>",unsafe_allow_html=True)
+
+
+            st.subheader("Naz after October 4th 2021... :tada: ")
+            st.write(
+                f"""<p><img src="https://media3.giphy.com/media/6nuiJjOOQBBn2/200w.webp?cid=ecf05e47xcmnv9n8vasc1d01665fpyw6u8qee6tg305wyews&rid=200w.webp&ct=g" style="width:500px;height:450px;"></p>""",
                 unsafe_allow_html=True,
             )
             st.write("\n")
@@ -210,29 +226,82 @@ def page_content(page_number):
 
     elif page_number == 5:
         st.header("""Messages from your fellow JPS-ians! :man-woman-girl-boy:""")
-        
+        st.write("<br>",unsafe_allow_html=True)
+        st.write("<br>",unsafe_allow_html=True)
         # st.write(jps_messages)
         count = 0
         for message, people in jps_messages.items():
-            cols_to_write = st.columns([1,3,3,1])
-            if count % 2 == 0:
-                col_picked = 1
-            else:
-                col_picked = 2
-            cols_to_write[col_picked].write(
-                f"""<span style="font-size:18px"> {message.strip()}</span>  -  <span style="color:#FFC0CB;font-size:20px"> <b>{people}</b></span>""",
-                unsafe_allow_html=True,
-            )
-            count +=1
-            # cols[7].write(
-            #     f"""<i><span style="font-size:18px"> (Likelihood Not Selected) </span></i>""",
-            #     unsafe_allow_html=True,
-            # )
+            if people == 'Li Ming':
+                cols_to_write = st.columns ([1,6,1.5])
+                cols_to_write[1].write(
+                    f"""<span style="font-size:26px;color:#FFCC66"> {message.strip()}</span> <span style="color:#FFC0CB;font-size:28px;text-align:right"> - <b><i>{people}</i></b></span>""",
+                    unsafe_allow_html=True,
+                )
+                # cols_to_write[1].write(
+                #     f""" <p style="color:#FFC0CB;font-size:28px;text-align:right"> - <b><i>{people}</i></b></p>""",
+                #     unsafe_allow_html=True,
+                # )
+        st.write("<br>",unsafe_allow_html=True)
+        st.write("<br>",unsafe_allow_html=True)
+        for message, people in jps_messages.items():
+            if people != 'Li Ming':
+                cols_to_write = st.columns([1,3,3,1.5])
+                if count % 2 == 0:
+                    col_picked = 1
+                    # cols_to_write[col_picked].write(
+                    #     f"""<p style="font-size:20px;"> {message.strip()}</p>""",
+                    #     unsafe_allow_html=True,
+                    # )
+                    # cols_to_write[col_picked].write(
+                    #     f""" <p style="color:#FFC0CB;font-size:20px;text-align:right"> - <b>{people}</b></p>""",
+                    #     unsafe_allow_html=True,
+                    # )
+                else:
+                    col_picked = 2
+                    # cols_to_write[col_picked].write(
+                    #     f"""<p style="font-size:20px;text-align:right"> {message.strip()}</p>""",
+                    #     unsafe_allow_html=True,
+                    # )
+                    # cols_to_write[col_picked].write(
+                    #     f""" <p style="color:#FFC0CB;font-size:20px;text-align:right"> - <b>{people}</b></p>""",
+                    #     unsafe_allow_html=True,
+                    # )
+                cols_to_write[col_picked].write(
+                    f"""<span style="font-size:20px"> {message.strip()}</span>  -  <span style="color:#FFC0CB;font-size:20px"> <b><i>{people}</i></b></span>""",
+                    unsafe_allow_html=True,
+                )
+
+                count +=1
 
     elif page_number == 6:
         st.header("Unable to meet because of MCO... but we still have these!")
-        pp = Image.open('naz_photos/naz_office.jpg')
-        st.image(pp, width=350)
+
+        #### Naz general photos
+        cols = st.columns([1,1,1,0.5,2])
+        naz_office = Image.open('naz_photos/naz_office.jpg')
+        cols[0].image(naz_office, width=500)
+        naz_pp = Image.open('naz_photos/naz.png')
+        cols[2].image(naz_pp, width=280)
+        gsy4 = Image.open('naz_photos/gsy4.jpeg')
+        cols[4].image(gsy4, width=580)
+
+
+        #### DSA
+        cols = st.columns([1.8,2.5])
+        naz_flower = Image.open('naz_photos/naz_flower.png')
+        cols[0].image(naz_flower, width=600)
+        dsa_flower = Image.open('naz_photos/dsa_flower.png')
+        cols[1].image(dsa_flower, width=1020)
+        #### With GSY
+        cols = st.columns([1,1,1])
+        gsy1 = Image.open('naz_photos/gsy1.jpeg')
+        cols[0].image(gsy1, width=500)
+        gsy2 = Image.open('naz_photos/gsy2.jpeg')
+        cols[1].image(gsy2, width=500)
+        gsy3 = Image.open('naz_photos/gsy3.jpeg')
+        cols[2].image(gsy3, width=500)
+        # gsy4 = Image.open('naz_photos/gsy4.jpeg')
+        # cols[3].image(gsy4, width=400)
 
 if 'page_num' not in st.session_state:
     st.session_state['page_num'] = 0
